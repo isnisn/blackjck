4a1444429d5e8bbe8f30047505daf5807ca4178f
Andreas Nilsson <an224qi@student.lnu.se>
2024-06-28 22:07:27 +0200

    Print the hand

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 
 #include "Deck.hpp"
-#include "IDeck.hpp"
+#include "Hand.hpp"
 
 int main() {
 
-  std::unique_ptr<IDeck<int, char>> deck = std::make_unique<Deck<int, char>>();
+  std::unique_ptr<Deck<int, char>> deck = std::make_unique<Deck<int, char>>();
   deck->print_deck();
   deck->shuffle();
   deck->print_deck();
+  Hand<5> hand(deck);
+  std::cout << "Printing hand" << std::endl;
+  std::cout << "Hand size: " << hand.get_hand_size() << std::endl;
+  hand.print_hand();
 
   return 0;
 }
