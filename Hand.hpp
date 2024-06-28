1ec7a34f458b268d8157861be7f806a12b91f67b
Andreas Nilsson <an224qi@student.lnu.se>
2024-06-28 22:07:16 +0200

    Refactor construc to take a ptr to deck

--- Hand.hpp
+++ Hand.hpp
@@ -1,3 +1,5 @@
+#ifndef HAND_HPP
+#define HAND_HPP
 
 #include "Card.hpp"
 #include "Deck.hpp"
@@ -12,13 +14,26 @@
  */
 template <size_t N> class Hand {
 public:
-  Hand(Deck<int, char> &deck) { hand = deck.generate_hand(N); }
+  Hand(std::unique_ptr<Deck<int, char>> &deck) {
+    hand = deck->generate_hand(N);
+  }
   ~Hand() { std::cout << "Destroyed hand" << std::endl; }
 
   size_t get_hand_size() const { return hand.size(); }
   Hand get_hand() const { return *this; }
 
+  /**
+   * @brief Get card from hand
+   *
+   * @param index
+   * @return Card<int, char>
+   */
   Card<int, char> get_card(const size_t &index) const { return *hand[index]; }
+
+  /**
+   * @brief Print hand
+   *
+   */
   void print_hand() const {
     for (const auto &c : hand) {
       std::cout << c->get_suit() << " " << c->get_value() << std::endl;
@@ -29,4 +44,4 @@ private:
   const size_t max_hand_size = N;
   std::vector<std::unique_ptr<Card<int, char>>> hand;
 };
-
+#endif
