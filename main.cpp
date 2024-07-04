e1ae51d79a4f61b2142d6d8e87c47e96014cf0f8
Andreas Nilsson <an224qi@student.lnu.se>
2024-07-04 21:34:45 +0200

    Cleanup main

--- main.cpp
+++ main.cpp
@@ -1,18 +1,9 @@
-#include "include/Deck/Deck.hpp"
-#include "include/Deck/DeckService.hpp"
 #include "include/Game/Game.hpp"
 #include "include/GameStyles/European.hpp"
-#include "include/Hand.hpp"
-#include "include/Player.hpp"
 #include <memory>
-#include <vector>
 
 int main() {
 
   Game game(std::make_unique<European>());
-  DeckService deck_service;
-
   game.start_game();
-
-  return 0;
 }
