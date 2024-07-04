3987c347e9a8d2f8ae8716c899753048d121e746
Andreas Nilsson <an224qi@student.lnu.se>
2024-07-04 21:57:33 +0200

    Main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include "include/Game/Game.hpp"
+#include "include/GameStyles/American.hpp"
 #include "include/GameStyles/European.hpp"
+#include "include/UI.hpp"
 #include <memory>
 
 int main() {
 
-  Game game(std::make_unique<European>());
-  game.start_game();
+  UI::getInstance().print("Welcome to the game!", "Dealer");
+
+  std::unique_ptr<IGameStyle> game_style = std::make_unique<European>();
+  Game game(std::move(game_style));
 }
