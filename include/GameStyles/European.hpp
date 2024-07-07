f740cd1a27bafae3225aa0696baba1812f619a1e
Andreas Nilsson <an224qi@student.lnu.se>
2024-07-07 10:56:29 +0200

    Different prinouts and rules

--- include/GameStyles/European.hpp
+++ include/GameStyles/European.hpp
@@ -1,6 +1,8 @@
 
 #include "../Deck/Deck.hpp"
 #include "../Game/IGameStyle.hpp"
+#include "../Player.hpp"
+#include "../PlayerService.hpp"
 #include <cstdint>
 #include <memory>
 
@@ -10,21 +12,38 @@
 class European : public IGameStyle {
 private:
   uint8_t num_decks = 2;
-  std::shared_ptr<Deck<int, char>> deck;
+  std::unique_ptr<Deck<int, char>> deck;
+  std::vector<std::shared_ptr<Player>> players;
+  PlayerService player_service;
   // .. other rules
 
 public:
   European() { this->setup(); }
+
   void play() override {
-    std::cout << "Starting a European style game of blackjack!" << std::endl;
+
+    UI::getInstance().print(m_Rules, "Game", YELLOW);
+    UI::getInstance().print("Starting a European style game of blackjack!",
+                            "Dealer");
     deck->shuffle();
-    // deck->print_deck();
+    deck->print_deck();
     return;
   }
 
 private:
+  std::string m_Rules =
+      "The European style of blackjack is played with 2 decks of cards. "
+      "The dealer stands on a soft 17. The player can double down on any "
+      "hand, and can split up to 3 times. The player can also take "
+      "insurance if the dealer's face up card is an ace. The player can "
+      "surrender at any time. The player wins if they have a higher hand "
+      "than the dealer without going over 21.";
+
   void setup() override {
     // Init deck and so on
-    deck = std::make_shared<Deck<int, char>>(num_decks);
+    deck = std::make_unique<Deck<int, char>>(num_decks);
+
+    // Create players
+    player_service.create_players(3, players);
   }
 };
