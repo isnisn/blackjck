c2d7bccd73c108191bb200f299c4b31dc3997845
Andreas Nilsson <an224qi@student.lnu.se>
2024-07-09 15:42:14 +0200

    Depend on abstraction

--- include/PlayerService.hpp
+++ include/PlayerService.hpp
@@ -5,14 +5,14 @@
 class PlayerService {
 public:
   void create_players(int num_players,
-                      std::vector<std::shared_ptr<Player>> &players) {
+                      std::vector<std::shared_ptr<IPlayer>> &players) {
     for (int i = 0; i < num_players; ++i) {
-      std::shared_ptr<Player> player = create_player();
+      std::shared_ptr<IPlayer> player = create_player();
       players.emplace_back(player);
     }
   }
 
-  std::shared_ptr<Player> create_player() {
+  std::shared_ptr<IPlayer> create_player() {
     std::string name = UI::getInstance().get_input("Enter player name: ");
     return std::make_shared<Player>(name);
   }
